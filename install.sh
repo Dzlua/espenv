28377aef14e87dc04ccc848538617266c13dcbe0
dzlua <505544956@qq.com>
2019-07-15 22:06:06 +0800

    fixed bug of pip requirement

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-readonly FILE32=https://dl.espressif.com/dl/xtensa-lx106-elf-linux32-1.22.0-88-gde0bdc1-4.8.5.tar.gz
-readonly FILE64=https://dl.espressif.com/dl/xtensa-lx106-elf-linux64-1.22.0-88-gde0bdc1-4.8.5.tar.gz
+readonly FILE32=https://dl.espressif.com/dl/xtensa-lx106-elf-linux32-1.22.0-92-g8facf4c-5.2.0.tar.gz
+readonly FILE64=https://dl.espressif.com/dl/xtensa-lx106-elf-linux64-1.22.0-92-g8facf4c-5.2.0.tar.gz
 readonly BIN=xtensa-lx106-elf/bin
 
 if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
@@ -16,6 +16,9 @@ FILENAME=`basename $FILE`
 sudo apt install gcc git wget make libncurses5-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future
 
 echo -e
+
+pip install --upgrade pip
+pip install -r ./ESP8266_RTOS_SDK/requirements.txt
 
 # 安装编译工具
 if [ ! -d "./esp8266_toolchain" ]; then
